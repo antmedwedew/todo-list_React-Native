ded197bc6796bfcd8663b2623946159dce1c679a
Anton Medwedew <medwedew.anton2017@yandex.ru>
2020-09-22 18:19:53 +0300

    add_image_in_todos_list

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,23 +1,45 @@
 import React from 'react'
-import { View, StyleSheet, FlatList } from 'react-native'
+import { View, StyleSheet, FlatList, Image } from 'react-native'
 import { AddTodo } from '../components/AddTodo'
 import { Todo } from '../components/Todo'
 
 export const MainScreen = ({ addTodo, todos, removeTodo, openTodo }) => {
+
+  let content = (
+    <FlatList
+      keyExtractor={item => item.id.toString()} 
+      data={todos}
+      renderItem={({ item })=> (
+        <Todo todo={item} onRemove={removeTodo} onOpen={openTodo}/>)}
+    />
+  )
+
+  if (todos.length === 0) {
+    content = (
+      <View style={styles.imgWrapp}>
+        <Image style={styles.image} source={require('../../assets/no-items.png')} />
+      </View>
+    )
+  }
+
   return (
     <View>
       <AddTodo onAddTodo={addTodo} />
 
-      <FlatList
-        keyExtractor={item => item.id.toString()} 
-        data={todos}
-        renderItem={({ item })=> (
-          <Todo todo={item} onRemove={removeTodo} onOpen={openTodo}/>)}
-      />
+      {content}
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    
+  imgWrapp: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 300
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  }
 })
